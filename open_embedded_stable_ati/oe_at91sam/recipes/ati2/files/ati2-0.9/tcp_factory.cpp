6e8b453a193904690a8bfb51a891226264f74cc9
Nathan Bell <fishybell@gmail.com>
2010-10-04 23:28:46 +0000

    fixed debug messages, added resubscribe message, made tnums recycle

--- open_embedded_stable_ati/oe_at91sam/recipes/ati2/files/ati2-0.9/tcp_factory.cpp
+++ open_embedded_stable_ati/oe_at91sam/recipes/ati2/files/ati2-0.9/tcp_factory.cpp
@@ -3,9 +3,11 @@ using namespace std;
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/epoll.h>
+#include <errno.h>
 #include "tcp_factory.h"
+#include "timeout.h"
+#include "serial.h"
 #include "common.h"
-#include "errno.h"
 
 #define MAX_CONN 1200
 
@@ -17,10 +19,12 @@ FUNCTION_START("::FASIT_TCP_Factory(char *destIP, int port) : Connection(0xDEADB
    server.sin_port = htons(port);
 DMSG("Created addr with ip %s, (%08x) port %i\n", inet_ntoa(server.sin_addr), server.sin_addr, port);
 
-   // this is incrimented before use
-   last_tnum = 0;
-
    setTnum(UNASSIGNED); // so unassigned tnums point to this as the source
+
+   // after 125 milliseconds, send the resubscribe messag
+   Timeout::setMaxWait(125);
+   Timeout::addTimeoutEvent(RESUBSCRIBE);
+
 FUNCTION_END("::FASIT_TCP_Factory(char *destIP, int port) : Connection(0xDEADBEEF)")
 }
 
@@ -28,6 +32,18 @@ FASIT_TCP_Factory::~FASIT_TCP_Factory() {
 FUNCTION_START("::~FASIT_TCP_Factory()")
 FUNCTION_END("::~FASIT_TCP_Factory()")
 }
+
+// tell all downrange units to resubscribe
+void FASIT_TCP_Factory::SendResubscribe() {
+FUNCTION_START("::Send_Resubscribe")
+   ATI_16008 msg;
+   msg.sequence = 0;
+   ATI_header hdr = FASIT::createHeader(16008, BASE_STATION, &msg, sizeof(ATI_16008));
+   SerialConnection::queueMsgAll(&hdr, sizeof(ATI_header));
+   SerialConnection::queueMsgAll(&msg, sizeof(ATI_16008));
+FUNCTION_END("::Send_Resubscribe")
+}
+
 
 FASIT_TCP *FASIT_TCP_Factory::newConn() {
 FUNCTION_START("::newConn()")
@@ -38,25 +54,26 @@ FUNCTION_START("::newConn()")
 FUNCTION_HEX("::newConn()", NULL)
       return NULL;
    }
+   IMSG("Created new TCP socket: %i\n", sock)
 
    // connect TCP socket to given IP and port
    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
       switch (errno) {
-         case EACCES : DMSG("errno: EACCES\n") break;
-         case EPERM : DMSG("errno: EPERM\n") break;
-         case EADDRINUSE : DMSG("errno: EADDRINUSE\n") break;
-         case EAFNOSUPPORT : DMSG("errno: EAFNOSUPPORT\n") break;
-         case EAGAIN : DMSG("errno: EAGAIN\n") break;
-         case EALREADY : DMSG("errno: EALREADY\n") break;
-         case EBADF : DMSG("errno: EBADF\n") break;
-         case ECONNREFUSED : DMSG("errno: ECONNREFUSED\n") break;
-         case EFAULT : DMSG("errno: EFAULT\n") break;
-         case EINPROGRESS : DMSG("errno: EINPROGRESS\n") break;
-         case EINTR : DMSG("errno: EINTR\n") break;
-         case EISCONN : DMSG("errno: ISCONN\n") break;
-         case ENETUNREACH : DMSG("errno: ENETUNREACH\n") break;
-         case ENOTSOCK : DMSG("errno: ENOTSOCK\n") break;
-         case ETIMEDOUT : DMSG("errno: ETIMEDOUT\n") break;
+         case EACCES : IERROR("errno: EACCES\n") break;
+         case EPERM : IERROR("errno: EPERM\n") break;
+         case EADDRINUSE : IERROR("errno: EADDRINUSE\n") break;
+         case EAFNOSUPPORT : IERROR("errno: EAFNOSUPPORT\n") break;
+         case EAGAIN : IERROR("errno: EAGAIN\n") break;
+         case EALREADY : IERROR("errno: EALREADY\n") break;
+         case EBADF : IERROR("errno: EBADF\n") break;
+         case ECONNREFUSED : IERROR("errno: ECONNREFUSED\n") break;
+         case EFAULT : IERROR("errno: EFAULT\n") break;
+         case EINPROGRESS : IERROR("errno: EINPROGRESS\n") break;
+         case EINTR : IERROR("errno: EINTR\n") break;
+         case EISCONN : IERROR("errno: ISCONN\n") break;
+         case ENETUNREACH : IERROR("errno: ENETUNREACH\n") break;
+         case ENOTSOCK : IERROR("errno: ENOTSOCK\n") break;
+         case ETIMEDOUT : IERROR("errno: ETIMEDOUT\n") break;
       }
 FUNCTION_HEX("::newConn()", NULL)
       return NULL;
@@ -67,8 +84,8 @@ HERE
    // create new FASIT_TCP (with predefined tnum) and to our epoll list
    struct epoll_event ev;
    memset(&ev, 0, sizeof(ev));
-   FASIT_TCP *tcp = new FASIT_TCP(sock, ++last_tnum);
-DMSG("Created new connection %i with tnum %i\n", tcp-getFD(), tcp->getTnum())
+   FASIT_TCP *tcp = new FASIT_TCP(sock, findNextTnum());
+DMSG("Created new connection %i with tnum %i\n", tcp->getFD(), tcp->getTnum())
    ev.events = EPOLLIN;
    ev.data.ptr = (void*)tcp;
    if (epoll_ctl(efd, EPOLL_CTL_ADD, sock, &ev) < 0) {
@@ -80,4 +97,25 @@ FUNCTION_HEX("::newConn()", NULL)
    // return the result
 FUNCTION_HEX("::newConn()", tcp)
    return tcp;
+}
+
+// look for the lowest available tnum
+int FASIT_TCP_Factory::findNextTnum() {
+FUNCTION_START("::findNextTnum()")
+   // loop through all known tcp connections finding the lowest unused tnum
+   __uint16_t lowTnum = 0;
+   FASIT_TCP *tcp = FASIT_TCP::getFirst();
+   while (tcp != NULL) {
+      lowTnum = max(lowTnum, tcp->getTnum());
+      tcp = tcp->getNext();
+   }
+   if (lowTnum > 0 && FASIT_TCP::findByTnum(lowTnum - 1) == NULL) {
+      // there exists a hole in the tnum list, fill it from the top down
+      lowTnum--;
+   } else {
+      // there are no holes in the list, the next tnum is the lowest available
+      lowTnum++;
+   }
+FUNCTION_INT("::findNextTnum()", lowTnum)
+   return lowTnum;
 }
