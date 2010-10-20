a99be8fa02a37d1b5503888a48983c0c09a02888
Nathan Bell <fishybell@gmail.com>
2010-10-20 22:06:04 +0000

    added windows program for installation on the hand held computer or other computer acting as the main TRACR/FASIT Base Station computer. This program sends out a hearbeat Broadcast UDP packet over the network to be received by the base station computer. This is only need where a UHF/VHF radio is used.

---
+++ windows/BaseStation/BaseStation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BaseStation
+{
+    // main class just starts the work
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a thread to do all the work
+            Thread thread = new Thread(new ThreadStart(Work.DoWork));
+            thread.Start();
+        }
+    }
+
+    // worker class does the actual work (typical capitalist program)
+    class Work
+    {
+        Work() { }
+
+        public static void DoWork()
+        {
+            // create an IP endpoint for our broadcast packet
+            int groupPort = 53530;
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Parse("255.255.255.255"), groupPort);
+
+            // create and open a socket
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+
+            // loop forever
+            while (true)
+            {
+                // send
+                socket.SendTo(System.Text.ASCIIEncoding.ASCII.GetBytes("Base Station"), groupEP);
+
+                // sleep 10 seconds
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
