e66198d9ae2fe4f954981baf7e93a4423e7d8d6c
Nathan Bell <fishybell@gmail.com>
2011-03-15 00:51:41 +0000

    fixed eeprom.c to properly write when improperly read from stdin. also commented code better

--- open_embedded_stable_ati/oe_at91sam/recipes/ati2/files/ati2-0.9/eeprom.c
+++ open_embedded_stable_ati/oe_at91sam/recipes/ati2/files/ati2-0.9/eeprom.c
@@ -5,79 +5,77 @@
 #define WRITE 1
 #define ERROR 2
 
-static char *dirName[] = {
-    "read",
-    "write",
-    "error"
-};
-
 #define MAX_SIZE 512
 
 int main(int argc, char *argv[]) {
 
-    int i, retval, addr=0x0, size=0x40;
+    int i, retval, fd, addr=0x0, size=0x40;
     int direction = ERROR;
 
     char buf[MAX_SIZE];
 
-    char *file = "/sys/class/i2c-adapter/i2c-0/0-0050/eeprom";
+    char *file = "/sys/class/i2c-adapter/i2c-0/0-0050/eeprom"; // 0x50 is the eeprom's i2c address
     char *usage = "Usage: %s (read|write) [-addr ##] [-size ##] [-file XX]\n";
 
     // loop over arguments
     for (i=1; i<argc; i++) {
        if((retval=strncmp("write", argv[i], 5)) == 0) {
+           // we're going to write
            direction = WRITE;
        } else if((retval=strncmp("read", argv[i], 4)) == 0) {
+           // we're going to read
            direction = READ;
         } else if((retval=strncmp("-file", argv[i], 5)) == 0 && (i+1<argc)) {
+           // we have a different address for the eeprom device
            file = argv[++i];
        } else if((retval=strncmp("-addr", argv[i], 5)) == 0 && (i+1<argc)) {
            i++;
+           // read address as hex
            if(argv[i][0] == '0' && (argv[i][1] == 'x' || argv[i][1] == 'X')) {
                if((retval=sscanf(argv[i]+2,"%x",&addr)) != 1) {
                    error_at_line(-1,0,__FILE__,__LINE__,"invalid hexadecimal number for address: %s\n", argv[i]);
-                   return -1;
                }
            } else {
+               // read address as integer
                if((retval=sscanf(argv[i],"%i",&addr)) != 1) {
+                   // read address as hex
                    if((retval=sscanf(argv[i],"%x",&addr)) != 1) {
                        error_at_line(-1,0,__FILE__,__LINE__,"invalid number for address: %s\n", argv[i]);
-                       return -1;
                    }
                }
            }
        } else if((retval=strncmp("-size", argv[i], 5)) == 0 && (i+1<argc)) {
            i++;
+           // read size as hex
            if(argv[i][0] == '0' && (argv[i][1] == 'x' || argv[i][1] == 'X')) {
                if((retval=sscanf(argv[i]+2,"%x",&size)) != 1) {
                    error_at_line(-1,0,__FILE__,__LINE__,"invalid hexadecimal number for size: %s\n", argv[i]);
-                   return -1;
                }
            } else {
+               // read size as integer
                if((retval=sscanf(argv[i],"%i",&size)) != 1) {
+                   // read size as hex
                    if((retval=sscanf(argv[i],"%x",&size)) != 1) {
                        error_at_line(-1,0,__FILE__,__LINE__,"invalid number for size: %s\n", argv[i]);
-                       return -1;
                    }
                }
            }
+           // don't overflow our buffer
            if(size > MAX_SIZE) {
                error_at_line(-1,0,__FILE__,__LINE__,"invalid number for size: 0x%20X (max size is %i)\n", size, MAX_SIZE);
            }
         } else {
+           // bad command line argument
            error_at_line(-1,0,__FILE__,__LINE__,usage, argv[0]);
-           return -1;
         }
     }
-
-    int fd;
 
     // error out or open the file for reading or writing
     switch (direction) {
         default:
         case ERROR:
+            // we didn't get a "read" or "write" on the command line
             error_at_line(-1,0,__FILE__,__LINE__,usage, argv[0]);
-            return -1;
             break;
 
         case READ:
@@ -89,11 +87,13 @@ int main(int argc, char *argv[]) {
             break;
     }
 
+    // check file descriptor
     if(fd == -1) {
         close(fd);
         error_at_line(-1,0,__FILE__,__LINE__,"Could open file %s\n", file);
     }
     
+    // move to addr address of eeprom
     if((retval=lseek(fd, addr, SEEK_SET)) != addr) {
         close(fd);
         error_at_line(-1,0,__FILE__,__LINE__,"Could not seek to address 0x%02X\n", addr);
@@ -101,23 +101,35 @@ int main(int argc, char *argv[]) {
 
     switch (direction) {
         case READ:
+            // read buffer from eeprom
             if((retval=read(fd, buf, size)) != size) {
                 close(fd);
                 error_at_line(-1,0,__FILE__,__LINE__,"Could not read full 0x%02X bytes\n", size);
             }
+            // write buffer to stdout
             fwrite(buf, sizeof(char), size, stdout);
             break;
         case WRITE:
+            // read buffer from stdin
             if((retval=fread(buf, sizeof(char), size, stdin)) != size) {
-                close(fd);
-printf("retval: %i\nbuf: %s\n", retval, buf);
-                error_at_line(-1,0,__FILE__,__LINE__,"Could not read full 0x%02X bytes from stdin\n", size);
+                // check for a read without a null-terminator
+                if (retval != size-1 && buf[retval-1] != '\0') {
+                    close(fd);
+                    error_at_line(-1,0,__FILE__,__LINE__,"Could not read full 0x%02X bytes from stdin\n", size);
+                } else {
+                    buf[retval] = '\0'; // null-terminate string
+                }
             }
+            // write buffer to eeprom
             if((retval=write(fd, buf, size)) != size) {
                 close(fd);
                 error_at_line(-1,0,__FILE__,__LINE__,"Could not write full 0x%02X bytes\n", size);
             }
             break;
     }
+
+    // clean up and exit
     close(fd);
+    return 0;
 }
+
