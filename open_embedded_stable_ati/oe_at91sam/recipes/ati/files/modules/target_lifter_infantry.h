8d3c6c432358ac8c57ce293ccf3c3e96dd7b8b09
Nathan Bell <fishybell@gmail.com>
2011-04-21 19:11:09 +0000

    unified location of constants

--- open_embedded_stable_ati/oe_at91sam/recipes/ati/files/modules/target_lifter_infantry.h
+++ open_embedded_stable_ati/oe_at91sam/recipes/ati/files/modules/target_lifter_infantry.h
@@ -3,4 +3,15 @@
 
 #include "target_hardware.h"
 
+#define LIFTER_POSITION_DOWN   			0
+#define LIFTER_POSITION_UP    			1
+#define LIFTER_POSITION_MOVING  		2
+#define LIFTER_POSITION_ERROR_NEITHER	3	// Neither limit switch is active, but the lifter is not moving
+
+// get current position
+extern int lifter_position_get(void);
+
+// set current position
+extern int lifter_position_set(int position);
+
 #endif // __TARGET_LIFTER_INFANTRY_H__
