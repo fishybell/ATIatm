1380cf03da3290d9fcf6f42f7472505c456d9ef2
Nathan Bell <fishybell@gmail.com>
2011-05-12 22:50:02 +0000

    added interface for intra-kernel communication

--- open_embedded_stable_ati/oe_at91sam/recipes/ati/files/modules/target_mover_generic.h
+++ open_embedded_stable_ati/oe_at91sam/recipes/ati/files/modules/target_mover_generic.h
@@ -3,4 +3,20 @@
 
 #include "target_hardware.h"
 
+// get current speed (positive/negative for direction)
+extern int mover_speed_get(void); // in whole mph
+
+// set current speed (positive/negative for direction)
+extern int mover_speed_set(int speed); // in whole mph
+
+// immediate stop (set to 0 will coast to a stop)
+extern int mover_speed_stop(void);
+
+// get current position (from home in feet)
+extern int mover_position_get(void);
+
+// register a callback for the lift event
+typedef void (*move_event_callback)(int); // called on finished, starting, and error (passing an EVENT_### value)
+extern void set_move_callback(move_event_callback handler);
+
 #endif // __TARGET_MOVER_GENERIC_H__
