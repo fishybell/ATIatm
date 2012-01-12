84fb54252aeabbd37de96f1578140923ea314e2c
Shelly Bjorkquist-Skuza <shellyb@actiontarget.com>
2012-01-12 21:25:07 +0000

    Made a temporary test class to listen for broadcast signals

---
+++ windows/EepromGUI/EepromGUI/MyTcpListener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EepromGUI
+{
+    class MyTcpListener
+    {
+        public MyTcpListener()
+        {
+            /*UdpClient client = new UdpClient(_localPort, AddressFamily.InterNetwork);
+            IPEndPoint groupEp = new IPEndPoint(IPAddress.Broadcast, _remotePort);
+            client.Connect(groupEp);
+
+            client.Send(_findBytes, _findBytes.Length);
+            client.Close();*/
+
+            IPEndPoint recvEp = new IPEndPoint(IPAddress.Any, 4227);
+            UdpClient udpResponse = new UdpClient(4227);
+            for (int i = 0; i < 10; i++)
+            {
+
+                Byte[] recvBytes = udpResponse.Receive(ref recvEp);
+                Console.WriteLine("Received: " + recvEp.Address);
+            }
+            
+        }
+
+
+    }
+}
