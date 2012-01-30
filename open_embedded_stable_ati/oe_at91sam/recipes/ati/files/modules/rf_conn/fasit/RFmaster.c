2d08c9851fc224f2144899adc0c2a8cc34837449
brad.tutch <>
2012-01-30 22:31:43 +0000

    *** empty log message ***

--- open_embedded_stable_ati/oe_at91sam/recipes/ati/files/modules/rf_conn/fasit/RFmaster.c
+++ open_embedded_stable_ati/oe_at91sam/recipes/ati/files/modules/rf_conn/fasit/RFmaster.c
@@ -4,188 +4,183 @@
 #include <sys/epoll.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h> /* POSIX terminal control definitions */
+
+#include "mcp.h"
+
+
+
+    /*
+     * 'open_port()' - Open serial port 1.
+     *
+     * Returns the file descriptor on success or -1 on error.
+     */
+
+int open_port(void){
+    int fd; /* File descriptor for the port */
+    char sport[]="/dev/ttyS0",buf[200];
+    
+    fd = open(sport, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd == -1) {
+       /*
+	* Could not open the port.
+	*/
+	strerror_r(errno,buf,200);
+	DCMSG(RED,"RFmaster open_port: Unable to open %s - %s \n", sport,buf);
+    } else {
+	fcntl(fd, F_SETFL, 0);
+	DCMSG(RED,"RFmaster serial port %s open and ready \n", sport);
+	
+    }
+    
+    return (fd);
+}
+
+
 
 
 // tcp port we'll listen to for new connections
-#define PORT 4004
+#define defaultPORT 4004
 
 // size of client buffer
 #define CLIENT_BUFFER 1024
 
-// kill switch to program
-static int close_nicely = 0;
-static void quitproc() {
-    close_nicely = 1;
+void DieWithError(char *errorMessage){
+    char buf[200];
+    strerror_r(errno,buf,200);
+    DCMSG(RED,"RFmaster %s %s \n", errorMessage,buf);
+    exit(1);
 }
+
+
+/*************
+ *************  HandleRF
+ *************
+ *************  We have a socket to the MCP,  and Serial connection to the RF
+ *************
+ *************  Just pass messages between the two...
+ *************  
+ *************  if we have a socket to an MCP, we act as a driver/link to the RF modem
+ *************  through our serial port.  for now the RFmaster will only forward comms in
+ *************  both directions - TCP connection to RF, and RF to TCP.
+ *************    At some point it might be best if the RFmaster does some data massaging,
+ *************  but until that is clear to me what it would need to do, for now we just pass comms
+ *************
+ *************/
+
+
+void HandleRF(int MCPsock,int RFfd){
+#define MbufSize 4096
+    char Mbuf[MbufSize];        /* Buffer MCP socket */
+    int MsgSize=1;                    /* Size of received message */
+
+/**   loop until we lose connection  **/
+    while(MsgSize){
+
+	/*   do a select to see if we have a message from either the RF or the MCP  */
+	/* then based on the source, send the message to the destination  */
+	
+    /* Receive message from MCP */
+    if ((MsgSize = recv(MCPsock, Mbuf, MbufSize, 0)) < 0)
+	DieWithError("recv() failed");
+
+    /* Send received string and receive again until end of transmission */
+    if (send(MCPsock, Mbuf, MsgSize, 0) != MsgSize)
+	DieWithError("send() failed");
+
+    }
+    
+    close(MCPsock);    /* Close socket */
+    
+}
+
+/*************
+ *************  RFmaster stand-alone program.
+ *************
+ *************  if there is no argument, use defaultPORT
+ *************  otherwise use the first are as the port number to listen on.
+ *************
+ *************  Also we first set up our connection to the RF modem on the serial port.
+ *************
+ *************  then we loop until we have a connection, and then when we get one we call the
+ *************  handleRF routine which does all the communicating.
+ *************    when the socket dies, we come back here and listen for a new MCP
+ *************    
+ *************/
 
 int main(int argc, char **argv) {
-    int retval, yes=1;
-    int client, listener; // socket file descriptors
-    socklen_t addrlen;
-    struct sockaddr_in serveraddr, local;
+    int serversock;			 /* Socket descriptor for server connection */
+    int MCPsock;			 /* Socket descriptor to use */
+    int RFfd;				 /* File descriptor for RFmodem serial port */
+    struct sockaddr_in ServAddr;	 /* Local address */
+    struct sockaddr_in ClntAddr;	 /* Client address */
+    unsigned short RFmasterport;	 /* Server port */
+    unsigned int clntLen;                /* Length of client address data structure */
 
-    // install signal handlers
-    signal(SIGINT, quitproc);
-    signal(SIGQUIT, quitproc);
-
-    // get the listener socket
-    if((listener = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("Server-socket() error ");
-
-        // just exit
-        return -1;
-    }
-
-    // "address already in use" error message
-    if(setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-        perror("Server-setsockopt() error ");
-        return -1;
-    }
-
-    // bind to socket
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = INADDR_ANY;
-    serveraddr.sin_port = htons(PORT);
-    memset(&(serveraddr.sin_zero), '\0', 8);
-
-    if(bind(listener, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1) {
-        perror("Server-bind() error ");
-        return -1;
-    }
-
-    // listen on socket
-    if(listen(listener, 10) == -1) {
-        perror("Server-listen() error ");
-        return -1;
-    }
-
-    // accept new clients
-    while (!close_nicely) {
-        addrlen = sizeof(local);
-        client = accept(listener, (struct sockaddr *) &local,
-                        &addrlen);
-        if(client < 0){
-            perror("accept");
-            close_nicely = 1;
-        }
-        int pid = fork();
-        if (pid == 0) {
-            // child process, break out of loop
-//printf("forked child\n");
-            close(listener);
-            break;
-        } else if (pid < 0) {
-            perror("fork");
-        }
-    }
-    // did the parent exit?
-    if (close_nicely) {
-        close(listener);
-        return 0;
-    }
-//printf("is child\n");
-
-    // set up epoll
-    struct epoll_event ev, events[MAX_EVENTS];
-    struct timeval tv;
-    int nfds; // number of file descriptors (returned in a single epoll_wait call)
-    efd = epoll_create(MAX_CONNECTIONS);
-
-    // add client socket to epoll
-    memset(&ev, 0, sizeof(ev));
-    setnonblocking(client);
-    ev.events = EPOLLIN;
-    ev.data.fd = client;
-    char *client_buf = malloc(CLIENT_BUFFER); // create a read buffer for the client
-    memset(client_buf, '\0', CLIENT_BUFFER);
-    if (epoll_ctl(efd, EPOLL_CTL_ADD, client, &ev) < 0) {
-        fprintf(stderr, "epoll insertion error: client\n");
-        return -1;
-    }
-
-    g_handle = handlecreate_nl_handle(client, ATI_GROUP);
-
-    // wait for netlink and socket messages
-    while(!close_nicely) {
-        // wait for response, timeout, or cancel
-        nfds = epoll_wait(efd, events, MAX_EVENTS, 30000); // timeout at 30 seconds
-
-        if (nfds == -1) {
-            /* select cancelled or other error */
-            fprintf(stderr, "select cancelled: "); fflush(stderr);
-            switch (errno) {
-                case EBADF: fprintf(stderr, "EBADF\n"); break;
-                case EFAULT: fprintf(stderr, "EFAULT\n"); break;
-                case EINTR: fprintf(stderr, "EINTR\n"); break;
-                case EINVAL: fprintf(stderr, "EINVAL\n"); break;
-                default: fprintf(stderr, "say what? %i\n", errno); break;
-            }
-            close_nicely = 1; // exit loop
-        } else if (nfds == 0) {
-            // timeout occurred
-//printf("child %i timed out\n", client);
-        } else {
-            int i, b, rsize;
-            for (i=0; i<nfds; i++) {
-                if (events[i].data.fd == nl_fd) {
-                    // netlink talking 
-//printf("nl\n");
-                    nl_recvmsgs_default(g_handle); // will call callback functions
-                } else if (events[i].data.fd == client) {
-                    // client talking
-//printf("sk %i:", client);
-                    b = strnlen(client_buf, CLIENT_BUFFER); // see where we left off
-                    // is there any space left in the buffer?
-//printf("%i", b);
-                    if (b >= CLIENT_BUFFER) {
-                        close_nicely = 1; // exit loop
-                    }
-                    // read client
-                    rsize = read(client, client_buf+b, CLIENT_BUFFER-b); // read into buffer at appropriate place, at most to end of buffer
-//printf(":%i\n", rsize);
-                    // parse buffer and send any netlink messages needed
-                    if (rsize == 0 || telnet_client(g_handle, client_buf, client) != 0) {
-//printf("sk %i closing\n", client);
-                        close_nicely = 1; // exit loop
-                    }
-                }
-            }
-        }
-    }
-    // the client is done
-    close(client); // close socket
-    free(client_buf); // free buffer
-
-    // destroy netlink handle
-    nl_handle_destroy(g_handle);
-
-    return 0;
-}
-
-// Verifies the input is a correctly formed MAC address
-int isMac(char* cmd) {
-	int arg1, arg2;
-	arg2 = 0;
-    int i = 0;
-
-	while(i <= 16) {
-		int hex = cmd[arg2+i];
-		if(i==2 || i==5 || i==8 || i==11 || i==14) {
-			// Check for the colon to be in the right place
-			if(hex != 58) {
-				return 0;
-			}
-		}	// Make sure all numbers fall within the hexidecimal range
-		else if ( (hex >= 48 && hex <= 58) || (hex >= 97 && hex <= 102) || (hex >= 65 && hex <= 70) ) {
-			// nothing in here
-		} else {
-			return 0;
-		}
-		i++;	
+    if (argc == 1){     /* Test for correct number of arguments */
+	RFmasterport = defaultPORT;
+	printf(" Listening on default port <%d>\n", RFmasterport);
+    } else if (argc != 2){     /* Test for correct number of arguments */
+	fprintf(stderr, "Usage:  %s <Server Port>\n", argv[0]);
+	exit(1);
+    } else {	
+	RFmasterport = atoi(argv[1]);
+	if ((RFmasterport<1)||(RFmasterport>65534)){
+	    printf(" Listening on default port <%d>\n", RFmasterport);
+	} else {
+	    fprintf(stderr, "Port out of range\n",RFmasterport);
+	    exit(1);
 	}
-	return 1;
+    }
+
+//   Okay,   set up the RF modem link here
+
+   RFfd=open_port(); 
+    
+
+//  now Create socket for the incoming connection */
+
+    if ((serversock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	DieWithError("socket() failed");
+
+	/* Construct local address structure */
+    memset(&ServAddr, 0, sizeof(ServAddr));   /* Zero out structure */
+    ServAddr.sin_family = AF_INET;                /* Internet address family */
+    ServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
+    ServAddr.sin_port = htons(RFmasterport);      /* Local port */
+
+	/* Bind to the local address */
+    if (bind(serversock, (struct sockaddr *) &ServAddr, sizeof(ServAddr)) < 0)
+	DieWithError("bind() failed");
+
+	/* Set the size of the in-out parameter */
+    clntLen = sizeof(ClntAddr);
+
+	/* Mark the socket so it will listen for incoming connections */
+	/* the '1' for MAXPENDINF might need to be a '0')  */
+    if (listen(serversock, 2) < 0)
+	DieWithError("listen() failed");
+
+    for (;;) {
+
+	/* Wait for a client to connect */
+	if ((MCPsock = accept(serversock, (struct sockaddr *) &ClntAddr,  &clntLen)) < 0)
+	    DieWithError("accept() failed");
+
+	/* MCPsock is connected to a Master Control Program! */
+
+	DCMSG(BLUE,"Good connection to MCP %s", inet_ntoa(ClntAddr.sin_addr));
+	HandleRF(MCPsock,RFfd);
+	DCMSG(BLUE,"Connection to MCP closed.   listening for a new MCPs");
+	
+    }
+    /* NOT REACHED */
 }
 
 
