a2f71eb57e4aade86e0f5d1f5c3440c577152830
Nathan Bell <fishybell@gmail.com>
2012-03-23 00:57:45 +0000

    added new states and tiemrs

--- open_embedded_stable_ati/oe_at91sam/recipes/ati/files/modules/rf_conn/fasit/minion_state.c
+++ open_embedded_stable_ati/oe_at91sam/recipes/ati/files/modules/rf_conn/fasit/minion_state.c
@@ -21,6 +21,8 @@ void minion_state(thread_data_t *minion, minion_time_t *mt, minion_bufs_t *mb) {
 
     long elapsed_tenths;
     int result;
+    LB_packet_t		LB_buf;
+    LB_status_req_t	*LB_status_req;
 	/***   what we do is use the minion's state_timer to determine how long to wait
 	 ***   set up above in the select.
 	 ***      if the select times out, we are good.
@@ -71,14 +73,62 @@ void minion_state(thread_data_t *minion, minion_time_t *mt, minion_bufs_t *mb) {
 	if (!minion->S.state_timer)  { // timer reached zero
 	//   We should only do the section if the next timer is really up, and
 	//   not just if another fd was ready about the same time
-	DCMSG(RED,"MINION %d: timer update.   exp.flags/timer=0x%x/%i   event.flags/timer=0x%x/%i",minion->mID,minion->S.exp.flags,minion->S.exp.timer,minion->S.event.flags,minion->S.event.timer);
+	DCMSG(RED,"MINION %d: timer update.   rf_t.flags/timer=0x%x/%i   exp.flags/timer=0x%x/%i   event.flags/timer=0x%x/%i",minion->mID,minion->S.rf_t.flags,minion->S.rf_t.timer,minion->S.exp.flags,minion->S.exp.timer,minion->S.event.flags,minion->S.event.timer);
 	
+	// if there is an rf timer flag set, we then need to do something
+	if (minion->S.rf_t.flags) {
+
+	    if (minion->S.rf_t.timer>elapsed_tenths){
+		minion->S.rf_t.timer-=elapsed_tenths;	// not timed out.  but decrement out timer
+		DCMSG(RED,"MINION %d: not timed out.   S.rf_t.timer=%d   S.rf_t.timer=%d",minion->mID,minion->S.rf_t.timer,minion->S.rf_t.timer);
+
+	    } else {	// timed out,  move to the next state, do what it should.
+		
+		switch (minion->S.rf_t.flags) {
+         case F_rf_t_waiting_short:
+            // break connection to FASIT server
+            // set "forget" flag for this minion
+            // clear waiting flags
+            minion->S.rf_t.flags = 0;
+            minion->S.rf_t.timer = 0;
+            break;
+
+         case F_rf_t_waiting_long:
+            // ask for a status
+		    LB_status_req  =(LB_status_req_t *)&LB_buf;	// make a pointer to our buffer so we can use the bits right
+		    LB_status_req->cmd=LBC_STATUS_REQ;
+		    LB_status_req->addr=minion->RF_addr;
+		    
+
+		    minion->S.status.flags=F_told_RCC;
+		    minion->S.status.timer=20;
+		    // calculates the correct CRC and adds it to the end of the packet payload
+		    // also fills in the length field
+		    set_crc8(LB_status_req);
+		    
+		    // now send it to the MCP master
+		    result=write(minion->mcp_sock,LB_status_req,RF_size(LB_status_req->cmd));
+		    //  sent LB
+		    if (verbose&D_RF){	// don't do the sprintf if we don't need to
+			sprintf(mb->hbuf,"Minion %d: LB packet to MCP address=%4d cmd=%2d msglen=%d result=%d",
+				minion->mID,minion->RF_addr,LB_status_req->cmd,RF_size(LB_status_req->cmd),result);
+			DDCMSG2_HEXB(D_RF,YELLOW,mb->hbuf,LB_status_req,RF_size(LB_status_req->cmd));
+		    }
+
+         // I'm expecting a response within 3 seconds
+         minion->S.rf_t.flags=F_rf_t_waiting_short;
+         minion->S.rf_t.timer=30; // give it three seconds
+            break;
+		}  // end of switch
+	    }   // else clause - rf_t flag
+	} // if clause - rf_t flag not set
+
 	// if there is an expose flag set, we then need to do something
 	if (minion->S.exp.flags!=F_exp_ok) {
 
 	    if (minion->S.exp.timer>elapsed_tenths){
 		minion->S.exp.timer-=elapsed_tenths;	// not timed out.  but decrement out timer
-		DCMSG(RED,"MINION %d: not timed out.   S.exp.timer=%d   S.event.timer=%d",minion->mID,minion->S.exp.timer,minion->S.event.timer);
+		DCMSG(RED,"MINION %d: not timed out.   S.exp.timer=%d   S.exp.timer=%d",minion->mID,minion->S.exp.timer,minion->S.exp.timer);
 
 	    } else {	// timed out,  move to the next state, do what it should.
 		
@@ -97,8 +147,8 @@ void minion_state(thread_data_t *minion, minion_time_t *mt, minion_bufs_t *mb) {
 		    case F_exp_expose_B:
 
 			minion->S.exp.data=90;	// make the current positon in movement
-			minion->S.exp.flags=F_exp_expose_C;	// we have reached the exposed position, now wait for confirmation from the RF slave
-			minion->S.exp.timer=9000;	// lots of time to wait for a RF reply
+			minion->S.exp.flags=F_exp_expose_C;	// we have reached the exposed position, now ask for an update
+			minion->S.exp.timer=15;	// 1.5 second later
 			DDCMSG(D_TIME,MAGENTA,"exp_B %06ld.%1d   elapsed_tenths=%ld 2102 simulated %d \n"
 			      ,mt->elapsed_time.tv_sec, (int)(mt->elapsed_time.tv_sec/100000000L),elapsed_tenths,minion->S.exp.data);
 			sendStatus2102(0,mb->header,minion); // forces sending of a 2102
@@ -106,8 +156,38 @@ void minion_state(thread_data_t *minion, minion_time_t *mt, minion_bufs_t *mb) {
 			break;
 
 		    case F_exp_expose_C:
-			// it timed out
+//		    also build an LB packet  to send
+		    LB_status_req  =(LB_status_req_t *)&LB_buf;	// make a pointer to our buffer so we can use the bits right
+		    LB_status_req->cmd=LBC_STATUS_REQ;
+		    LB_status_req->addr=minion->RF_addr;
+		    
+
+		    minion->S.status.flags=F_told_RCC;
+		    minion->S.status.timer=20;
+		    // calculates the correct CRC and adds it to the end of the packet payload
+		    // also fills in the length field
+		    set_crc8(LB_status_req);
+		    
+		    // now send it to the MCP master
+		    result=write(minion->mcp_sock,LB_status_req,RF_size(LB_status_req->cmd));
+		    //  sent LB
+		    if (verbose&D_RF){	// don't do the sprintf if we don't need to
+			sprintf(mb->hbuf,"Minion %d: LB packet to MCP address=%4d cmd=%2d msglen=%d result=%d",
+				minion->mID,minion->RF_addr,LB_status_req->cmd,RF_size(LB_status_req->cmd),result);
+			DDCMSG2_HEXB(D_RF,YELLOW,mb->hbuf,LB_status_req,RF_size(LB_status_req->cmd));
+		    }
+
+         // I'm expecting a response within 3 seconds
+         minion->S.rf_t.flags=F_rf_t_waiting_short;
+         minion->S.rf_t.timer=30; // give it three seconds
+
+         // try again and again until we're sent the conceal command
+			minion->S.exp.flags=F_exp_expose_D;	// we have reached the exposed position, now ask for an update
+			minion->S.exp.timer=0;	// no timeout, we don't actually handle _D, just look at it
 			break;
+
+		    case F_exp_expose_D:
+            break;
 
 		    case F_exp_conceal_A:
 			minion->S.exp.data=45;	// make the current positon in movement
@@ -121,8 +201,8 @@ void minion_state(thread_data_t *minion, minion_time_t *mt, minion_bufs_t *mb) {
 
 		    case F_exp_conceal_B:
 			minion->S.exp.data=0;	// make the current positon in movement
-			minion->S.exp.flags=F_exp_conceal_C;	// we have reached the exposed position, now wait for confirmation from the RF slave
-			minion->S.exp.timer=9000;	// lots of time to wait for a RF reply
+			minion->S.exp.flags=F_exp_conceal_C;	// we have reached the concealed position, ask for status update again in one second
+			minion->S.exp.timer=15;	// 1.5 second later
 			DDCMSG(D_TIME,MAGENTA,"conceal_B %06ld.%1d   elapsed_tenths=%ld 2102 simulated %d \n"
 			       ,mt->elapsed_time.tv_sec, (int)(mt->elapsed_time.tv_sec/100000000L),elapsed_tenths,minion->S.exp.data);
 			sendStatus2102(0,mb->header,minion); // forces sending of a 2102
@@ -130,7 +210,34 @@ void minion_state(thread_data_t *minion, minion_time_t *mt, minion_bufs_t *mb) {
 			break;
 
 		    case F_exp_conceal_C:
+//		    also build an LB packet  to send
+		    LB_status_req  =(LB_status_req_t *)&LB_buf;	// make a pointer to our buffer so we can use the bits right
+		    LB_status_req->cmd=LBC_STATUS_REQ;
+		    LB_status_req->addr=minion->RF_addr;
+		    
 
+		    minion->S.status.flags=F_told_RCC;
+		    minion->S.status.timer=20;
+		    // calculates the correct CRC and adds it to the end of the packet payload
+		    // also fills in the length field
+		    set_crc8(LB_status_req);
+		    
+		    // now send it to the MCP master
+		    result=write(minion->mcp_sock,LB_status_req,RF_size(LB_status_req->cmd));
+		    //  sent LB
+		    if (verbose&D_RF){	// don't do the sprintf if we don't need to
+			sprintf(mb->hbuf,"Minion %d: LB packet to MCP address=%4d cmd=%2d msglen=%d result=%d",
+				minion->mID,minion->RF_addr,LB_status_req->cmd,RF_size(LB_status_req->cmd),result);
+			DDCMSG2_HEXB(D_RF,YELLOW,mb->hbuf,LB_status_req,RF_size(LB_status_req->cmd));
+		    }
+
+         // I'm expecting a response within 3 seconds
+         minion->S.rf_t.flags=F_rf_t_waiting_short;
+         minion->S.rf_t.timer=30; // give it three seconds
+
+         // clear out expose status...I won't try again
+			minion->S.exp.flags=0;
+			minion->S.exp.timer=0;
 			break;
 
 		}  // end of switch
@@ -192,6 +299,7 @@ void minion_state(thread_data_t *minion, minion_time_t *mt, minion_bufs_t *mb) {
 	minion->S.state_timer = 900;	// put in a worst case starting value of 90 seconds
 	Set_Timer(minion->S.exp.timer);		// if arg is >0 and <timer, it is the new timer
 	Set_Timer(minion->S.event.timer);		// if arg is >0 and <timer, it is the new timer
+	Set_Timer(minion->S.rf_t.timer);		// if arg is >0 and <timer, it is the new timer
 //	Set_Timer(minion->S.speed.timer);		// if arg is >0 and <timer, it is the new timer
 	
 }
