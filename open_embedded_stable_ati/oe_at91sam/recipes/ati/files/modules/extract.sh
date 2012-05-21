71494d7d04f0291485222ff0a7626cb2cbe8754d
Randy Neumann <randyn@actiontarget.com>
2012-05-21 21:11:47 +0000

    Create a self extracting tar file

---
+++ open_embedded_stable_ati/oe_at91sam/recipes/ati/files/modules/extract.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+echo ""
+echo "Self Extracting Firmware"
+echo ""
+SKIP=`awk '/^__TARFILE_FOLLOWS__/ { print NR + 2; exit 0; }' $0`
+
+THIS=`pwd`/$0
+
+echo "Extracting Files"
+echo ""
+
+tail -n +$SKIP $THIS | tar -xz
+
+echo "Changing Owner and Group"
+echo ""
+chown root *
+chgrp root *
+
+echo "Copying files"
+echo ""
+mv *.arm /usr/bin
+mv /usr/bin/bcast_client /usr/bin/bcast_client.old
+mv bcast_client /usr/bin
+mv /usr/bin/bcast_server /usr/bin/bcast_server.old
+mv bcast_server /usr/bin
+mv /usr/bin/bit_button /usr/bin/bit_button.old
+mv bit_button /usr/bin
+mv /usr/bin/event_conn /usr/bin/event_conn.old
+mv event_conn /usr/bin
+mv /usr/bin/fasit_conn /usr/bin/fasit_conn.old
+mv fasit_conn /usr/bin
+mv /usr/bin/user_conn /usr/bin/user_conn.old
+mv user_conn /usr/bin
+
+echo "Finished Copying Files"
+echo ""
+
+echo "Restarting Device"
+echo ""
+sleep 1
+init 6
+exit 0
+
+__TARFILE_FOLLOWS__
+
