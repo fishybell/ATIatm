838623ad97826cdcfbede0cc3f9a3ffa041c3378
Randy Neumann <randyn@actiontarget.com>
2012-06-13 22:09:08 +0000

    Added radio conditional

--- open_embedded_stable_ati/oe_at91sam/recipes/ati2/files/atiradiotest.sh
+++ open_embedded_stable_ati/oe_at91sam/recipes/ati2/files/atiradiotest.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
 /usr/bin/dtxm_edit.arm -v f
-killall RFslave.new.arm
-killall slaveboss.arm
+if [ `/usr/bin/is_comm` == "radio" ] ; then
+   killall RFslave.new.arm
+   killall slaveboss.arm
+fi
 echo 'X' | /usr/bin/microcom -s 19200 /dev/ttyS1
 sleep 1
 echo 'X' | /usr/bin/microcom -s 19200 /dev/ttyS1
@@ -12,5 +15,5 @@ sleep 1
 echo 'X' | /usr/bin/microcom -s 19200 /dev/ttyS1
 sleep 1
 if [ `/usr/bin/is_comm` == "radio" ] ; then
-  init 6
+   init 6
 fi
