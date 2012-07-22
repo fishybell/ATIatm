542b48bf15e5486182cf2c651a61676d1a0f92b7
Nathan Bell <fishybell@gmail.com>
2012-07-22 20:37:18 +0000

    fixed bug in timing (here is where the real fix was - removing the static variable and making it a member of the minion_time_t struct)

--- open_embedded_stable_ati/oe_at91sam/recipes/ati/files/modules/rf_conn/fasit/timestamp.c
+++ open_embedded_stable_ati/oe_at91sam/recipes/ati/files/modules/rf_conn/fasit/timestamp.c
@@ -4,35 +4,34 @@
 //  CLOCK_MONOTONIC_RAW  doesn't work on the ARM
 
 
-void timestamp(struct timespec *elapsed_time, struct timespec *istart_time, struct timespec *delta_time){
-    static struct timespec last_time;
+void timestamp(minion_time_t *mt){
     struct timespec current_time;
 
 
     clock_gettime(CLOCK_MONOTONIC,&current_time);	// get a current time
-    delta_time->tv_sec=current_time.tv_sec;
-    delta_time->tv_nsec=current_time.tv_nsec;		// make a copy
-    elapsed_time->tv_sec=current_time.tv_sec;
-    elapsed_time->tv_nsec=current_time.tv_nsec;		// make another copy
+    mt->delta_time.tv_sec=current_time.tv_sec;
+    mt->delta_time.tv_nsec=current_time.tv_nsec;		// make a copy
+    mt->elapsed_time.tv_sec=current_time.tv_sec;
+    mt->elapsed_time.tv_nsec=current_time.tv_nsec;		// make another copy
 
     // elapsed time is current time - initial time
-    elapsed_time->tv_sec-=istart_time->tv_sec;	// get the seconds right
-    if (elapsed_time->tv_nsec<istart_time->tv_nsec){
-	elapsed_time->tv_sec--;			// carry a second over for subtracting
-	elapsed_time->tv_nsec+=1000000000L;	// carry a second over for subtracting
+    mt->elapsed_time.tv_sec-=mt->istart_time.tv_sec;	// get the seconds right
+    if (mt->elapsed_time.tv_nsec<mt->istart_time.tv_nsec){
+	mt->elapsed_time.tv_sec--;			// carry a second over for subtracting
+	mt->elapsed_time.tv_nsec+=1000000000L;	// carry a second over for subtracting
     }
-    elapsed_time->tv_nsec-=istart_time->tv_nsec;	// get the useconds right
+    mt->elapsed_time.tv_nsec-=mt->istart_time.tv_nsec;	// get the useconds right
 
     // now calculate the time since the last timestamp in addition to the initial time difference 
-    delta_time->tv_sec-=last_time.tv_sec;	// get the seconds right
-    if (delta_time->tv_nsec<last_time.tv_nsec){
-	delta_time->tv_sec--;			// carry a second over for subtracting
-	delta_time->tv_nsec+=1000000000L;	// carry a second over for subtracting
+    mt->delta_time.tv_sec-=mt->last_time.tv_sec;	// get the seconds right
+    if (mt->delta_time.tv_nsec<mt->last_time.tv_nsec){
+	mt->delta_time.tv_sec--;			// carry a second over for subtracting
+	mt->delta_time.tv_nsec+=1000000000L;	// carry a second over for subtracting
     }
-    delta_time->tv_nsec-=last_time.tv_nsec;	// get the useconds right
+    mt->delta_time.tv_nsec-=mt->last_time.tv_nsec;	// get the useconds right
 
-    last_time.tv_sec=current_time.tv_sec;
-    last_time.tv_nsec=current_time.tv_nsec;	// update our time
+    mt->last_time.tv_sec=current_time.tv_sec;
+    mt->last_time.tv_nsec=current_time.tv_nsec;	// update our time
 
 }
 
