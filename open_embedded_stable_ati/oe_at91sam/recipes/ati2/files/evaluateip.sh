a44852929d637d9000eee15e73529b3a7f8ff60f
Randy Neumann <randyn@actiontarget.com>
2012-10-11 15:58:40 +0000

    Need these for new networking model

---
+++ open_embedded_stable_ati/oe_at91sam/recipes/ati2/files/evaluateip.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Use be_static to set the IP address to correctly
+# be used in this file.
+# See /etc/network/interfaces file for eth0 and eth0:0
+
+# if no arguments fail, avahi will assign address
+if [ $# == 0 ]; then
+   exit 1
+fi
+
+add=`is_static`
+if [ "$add" == "0.0.0.0" -o "$add" == "" ]; then
+# if we want to be static, we need an address
+   if [ "$1" == "static" ]; then
+      exit 1
+   fi
+else
+# if we want to use dhcp, do not have an address
+   if [ "$1" == "dhcp" ]; then
+      exit 1
+   fi
+fi
+exit 0
