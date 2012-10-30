738575a09a717fad3d9ab54da3e59bfc364a3fd4
Shelly Bjorkquist-Skuza <shellyb@actiontarget.com>
2012-10-30 21:13:55 +0000

    Added the ability to flash multiple targets at the same time.

--- windows/EepromGUI/PMA/atifirmwarecopyall.bat
+++ windows/EepromGUI/PMA/atifirmwarecopyall.bat
@@ -3,6 +3,8 @@ REM Arguments: firmware username password [list of ips]
 
 set firmware=%1
 shift
+set shortFirmware=%1
+shift
 set username=%1
 shift
 set passwd=%1
@@ -15,9 +17,9 @@ if "%1"=="" GOTO Done
 echo "copying %firmware% to %1"
 ".\pscp.exe" -pw %passwd% %firmware% %username%@%1:/home/root
 
-".\plink.exe" -ssh -v -pw %passwd% %username%@%1 "chmod +x ./%firmware%"
+".\plink.exe" -ssh -v -pw %passwd% %username%@%1 "chmod +x ./%shortFirmware%"
 
-".\plink.exe" -ssh -v -pw %passwd% %username%@%1 "./%firmware%"
+".\plink.exe" -ssh -v -pw %passwd% %username%@%1 "./%shortFirmware%"
 
 Shift
 GOTO Start
