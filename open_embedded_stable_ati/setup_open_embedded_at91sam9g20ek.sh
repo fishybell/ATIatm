ca3f2fb531defed17612bdbbe49d70a0086cc092
Randy Neumann <randyn@actiontarget.com>
2013-05-09 14:23:39 +0000

    Changes for build env

--- open_embedded_stable_ati/setup_open_embedded_at91sam9g20ek.sh
+++ open_embedded_stable_ati/setup_open_embedded_at91sam9g20ek.sh
@@ -1,3 +1,4 @@
+#See below ##### for instructions on how to build with current settings
 # get bitbake, extract and create a symbolic link
 wget http://download.berlios.de/bitbake/bitbake-1.8.18.tar.gz
 tar zvxf bitbake-1.8.18.tar.gz
@@ -17,6 +18,33 @@ cd ..
 source ./oe_env.sh
 bitbake base-image
 bitbake console-at91sam9-image
+
+
+##### These instructions work for using current versions
+#This gets your source directories
+# Create a directory where you want your sources
+mkdir -p ~/Angstrom/oe
+#copy oe.tar where you want it.
+cp oe.tar ~/Angstrom/oe
+tar -xvf oe.tar
+
+# Change to your open_embedded_stable_ati directory
+cd ~/devel/atm/open_embedded_stable_ati
+
+#Create sim links to everything
+ln -s ~/Angstrom/oe/oe_sources oe_sources
+ln -s ~/Angstrom/oe/openembedded openembedded
+ln -s ~/Angstrom/oe/tmp tmp
+
+#Build images
+source ./oe_env.sh
+bitbake console-image
+bitbake u-boot
+
+#Or use bitbake_build just make sure to change dirname to your home directory
+vi bitbake_build
+./bitbake_build
+
 
 
 
